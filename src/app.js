43ee9a854d417d7bf6a3a6c1b2d02f9f5256d86a
MarkHiarley <hiarleycostamarley@gmail.com>
2025-07-25 14:18:52 -0300

    port

--- src/app.js
+++ src/app.js
@@ -82,7 +82,7 @@ app.delete('/cardapio/deletar/:id', async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(8000, () => {
+    console.log('Server is running on http://localhost:8000');
     console.log('Press Ctrl+C to stop the server');
 });
